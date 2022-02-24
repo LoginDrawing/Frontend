0036688e048f99cb49b58bdb63a1dbc5c1ddca3d
gusrns48 <gusrns48@naver.com>
2022-02-24 09:33:49 +0900

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, Routes, BrowserRoute as Router, BrowserRouter } from 'react-router-dom'
+import React, {Component} from 'react'
 import './App.css';
 import Header from './component/layout/header'
 import Footer from './component/layout/footer'
@@ -8,9 +9,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
-        <Route exact path="/" component={Home} />
-      </Router>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
