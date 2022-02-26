bed32981a38437263076a8de02ed76f6eecaa56e
Cat-Hanbit <hello.git.bo_yon_99@naver.com>
2022-02-26 19:28:48 +0900

    개인사이트 관리자 페이지 - 설정 탭 구현

--- js/private_site_manager.js
+++ js/private_site_manager.js
@@ -1,7 +1,7 @@
 const tabMenus = document.querySelectorAll('.tab_menu li');
 const tabContents = document.querySelectorAll('.content section');
 
-
+// 탭메뉴
 const activeSection = (e) => {
     e.stopPropagation();
 
@@ -20,10 +20,19 @@ const activeSection = (e) => {
     });
 }
 
-
 [...tabMenus][0].classList.add('active');
 [...tabContents][0].classList.add('active');
 
 tabMenus.forEach(menu => {
     menu.addEventListener('click', activeSection)
 })
+
+// 설정 > 공유링크 > 링크복사
+function linkCopy(){
+    var copyText = document.getElementById('copy_text');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("Copy");
+    alert('공유 링크가 성공적으로 복사되었습니다.');
+  
+}
