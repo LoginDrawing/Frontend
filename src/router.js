193bc00fcc442b4a1a7a874441052a003224b533
Cat-Hanbit <hello.git.bo_yon_99@naver.com>
2022-04-29 22:48:50 +0900

    추가

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import HomaPage from "./components/Homepage";
 import Manager from "./components/PrivateSiteManager";
+import Kakao from "./components/auth/kakao";
+import TestPage from "./components/TestPage";
 
 function Router() {
   return (
@@ -10,6 +12,8 @@ function Router() {
         <Route path = "/login" element={<Login />} />
         <Route path = "/home" element={<HomaPage />} />
         <Route path="/" element={<Manager />} />
+        <Route path = "/auth/kakao/callback" element={<Kakao />} />
+        <Route path = "/test" element={<TestPage />} />
       </Routes>
     </BrowserRouter>
   )
